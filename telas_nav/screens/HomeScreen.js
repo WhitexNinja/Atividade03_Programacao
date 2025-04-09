d14330f57919e022364d9c776ffd89fbd0543e9b
WhitexNinja <>
2025-04-09 11:47:38 -0300

    Quase terminando a primeira parte

---
+++ telas_nav/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'react-native-elements';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+const HomeScreen = ({navigation}) => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+};
---
+++ telas_nav/screens/LoginScreen.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
+
+const LoginScreen = ({navigation}) => {
+    return (
+        <View>
+            <Text>Tela de Login</Text>
+            <Button type='solid' style={{backgroundColor: 'dodgerblue'}} onPress={() => navigation.navigate('Home')}></Button>
+        </View>
+    )
+};
